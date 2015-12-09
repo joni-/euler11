5e05c8bc5e5b83926f7770a1b98658b25a2c8916
Joni Nevalainen <joninevalainen@gmail.com>
2015-12-09 21:21:46 +0200

    parse line into list of different combinations of numbers

--- euler11.py
+++ euler11.py
@@ -2,3 +2,18 @@ def parse_line(line):
     """ Parses given line into list of numbers.
     e.g. '1 2 3 4' => [1, 2, 3, 4] """
     return [int(num) for num in line.split()]
+
+
+def get_number_combinations(line, n):
+    """ Parses given line into lists of numbers of length n and returns them as
+    a list.
+    e.g. line: '1 2 3 4 5', n: 4 => [[1, 2, 3, 4], [2, 3, 4, 5]] """
+    numbers = parse_line(line)
+    result = []
+    start = 0
+    end = n
+    while end <= len(numbers):
+        result.append(numbers[start:end])
+        start += 1
+        end += 1
+    return result
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from euler11 import parse_line
+from euler11 import get_number_combinations, parse_line
 
 
 class ParsingTests(unittest.TestCase):
@@ -8,6 +8,13 @@ class ParsingTests(unittest.TestCase):
     def test_parse_numbers_from_line(self):
         self.assertEqual(parse_line("1 2 3 4"), [1, 2, 3, 4])
         self.assertEqual(parse_line("00 01 02 03"), [0, 1, 2, 3])
+
+    def test_list_of_numbers_from_line(self):
+        self.assertEqual(get_number_combinations("1 2 3 4", 4), [[1, 2, 3, 4]])
+        self.assertEqual(
+            get_number_combinations("1 2 3 4 5", 4),
+            [[1, 2, 3, 4], [2, 3, 4, 5]]
+        )
 
 
 if __name__ == "__main__":
